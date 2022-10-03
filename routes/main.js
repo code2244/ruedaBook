6ab42ab82522af37460c23448faba0b9ce261f03
Ricardo Hernandez <rmhm.photography@gmail.com>
2022-10-03 10:13:16 -0400

    Added a available class page

--- routes/main.js
+++ routes/main.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const todosController = require("../controllers/todos")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
+router.get("/todos", ensureAuth, todosController.getTodos);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+
 
 module.exports = router;
